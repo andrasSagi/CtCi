35428a8811df473a7248f61b7c83d5e8bb58a77f
andrasSagi <andras.sagi8128@gmail.com>
2019-01-29 14:25:21 +0100

    Add main class

---
+++ CtCiJ/src/main/java/ArraysAndStrings/ArraysAndStrings.java
@@ -0,0 +1,10 @@
+package ArraysAndStrings;
+
+class ArraysAndStrings {
+
+    static String URLify(String input, int length) {
+        char[] characters = new char[length];
+        
+        return "";
+    }
+}
