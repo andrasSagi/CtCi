262747041502c1a499c7027c4f58ecda4890dd13
andrasSagi <andras.sagi8128@gmail.com>
2019-01-29 14:26:08 +0100

    Create test for URLify

---
+++ CtCiJ/src/test/java/ArraysAndStrings/ArraysAndStringsTest.java
@@ -0,0 +1,15 @@
+package ArraysAndStrings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArraysAndStringsTest {
+
+    @Test
+    void testURLify() {
+        assertEquals("Mr%20John%20Smith",
+                ArraysAndStrings.URLify("Mr John Smith", 13));
+    }
+
+}
