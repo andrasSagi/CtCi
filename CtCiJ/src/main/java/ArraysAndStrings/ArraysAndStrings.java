1089e9ac9520f8688370cc7f1f13e09af2335840
andrasSagi <andras.sagi8128@gmail.com>
2019-01-29 14:41:13 +0100

    Implement URLify

--- CtCiJ/src/main/java/ArraysAndStrings/ArraysAndStrings.java
+++ CtCiJ/src/main/java/ArraysAndStrings/ArraysAndStrings.java
@@ -2,9 +2,19 @@ package ArraysAndStrings;
 
 class ArraysAndStrings {
 
-    static String URLify(String input, int length) {
-        char[] characters = new char[length];
-        
-        return "";
+    static String URLify(String input, int trueLength) {
+        char[] characters = new char[input.length()];
+        int index = 0;
+        for (int i=0; i < trueLength; i++) {
+            char current = input.charAt(i);
+            if (current == ' ') {
+                characters[index++] = '%';
+                characters[index++] = '2';
+                characters[index++] = '0';
+            } else {
+                characters[index++] = current;
+            }
+        }
+        return new String(characters);
     }
 }
