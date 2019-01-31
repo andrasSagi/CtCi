8933b645c97eecae3a3f908fbe2b9714af64163a
andrasSagi <andras.sagi8128@gmail.com>
2019-01-31 13:30:59 +0100

    Refactor oneAway

--- CtCiJ/src/main/java/ArraysAndStrings/ArraysAndStrings.java
+++ CtCiJ/src/main/java/ArraysAndStrings/ArraysAndStrings.java
@@ -42,35 +42,39 @@ class ArraysAndStrings {
     }
 
     static boolean oneAway(String string1, String string2) {
-        if (Math.abs(string1.length() - string2.length()) > 1) {
-            return false;
-        }
-        boolean editUsed = false;
         if (string1.length() == string2.length()) {
-            for (int i = 0; i < string1.length(); i++) {
-                if (string1.charAt(i) != string2.charAt(i) && !editUsed) {
-                    editUsed = true;
-                } else if (string1.charAt(i) != string2.charAt(i) && editUsed) {
-                    return false;
-                }
+            return checkSameLength(string1, string2);
+        } else if (string1.length() == string2.length() + 1) {
+            return checkDifferentLength(string1, string2);
+        } else if (string1.length() + 1 == string2.length()) {
+            return checkDifferentLength(string2, string1);
+        }
+        return false;
+    }
+
+    private static boolean checkSameLength(String string1, String string2) {
+        boolean editUsed = false;
+        for (int i = 0; i < string1.length(); i++) {
+            if (string1.charAt(i) != string2.charAt(i) && !editUsed) {
+                editUsed = true;
+            } else if (string1.charAt(i) != string2.charAt(i) && editUsed) {
+                return false;
             }
-        } else {
-            int index = 0;
-            String longer = string1;
-            String shorter = string2;
-            if (string1.length() < string2.length()) {
-                longer = string2;
-                shorter = string1;
-            }
-            for (int j = 0; j < shorter.length(); j++) {
-                if (longer.charAt(index) != shorter.charAt(j) && !editUsed) {
-                    editUsed = true;
-                    index++;
-                } else if (longer.charAt(index) != shorter.charAt(j) && editUsed) {
-                    return false;
-                }
+        }
+        return true;
+    }
+
+    private static boolean checkDifferentLength(String longer, String shorter) {
+        int index = 0;
+        boolean editUsed = false;
+        for (int j = 0; j < shorter.length(); j++) {
+            if (longer.charAt(index) != shorter.charAt(j) && !editUsed) {
+                editUsed = true;
                 index++;
+            } else if (longer.charAt(index) != shorter.charAt(j) && editUsed) {
+                return false;
             }
+            index++;
         }
         return true;
     }
